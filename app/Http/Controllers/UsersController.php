941db9be7a84ba71f9861c67def395325a362634
vagrant <vagrant@homestead>
2018-07-04 14:13:57 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -11,6 +12,32 @@ class UsersController extends Controller
 
         return view('users.create');
 
+
+    }
+    public function show(User $user){
+
+        return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+             'name'=>'required|max:50',
+             'email'=>'required|unique:users|email',
+             'password'=>'required|min:6',
+             'password_confirmation'=>'same:password'
+
+
+        ]);
+
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]
+        );
+        
+        session()->flash('success','Welcome!');
+        return redirect()->route('users.show',[$user]);
 
     }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,6 +10,7 @@
     @include('layouts._header')
 
     <div class="container">
+        @include('shared._message')
         @yield('content') @include('layouts._footer')
  
 
