06182b9e9a24d05e3c6c07bf6a753e001b819bc7
vagrant <vagrant@homestead>
2018-07-29 05:47:57 +0000

    Finished

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,11 +42,7 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    public function feed(){
-    
-        return $this->statuses()->orderBy('created_at','desc');
-
-    }
+  
 
     public function followers(){
 
@@ -77,8 +73,17 @@ class User extends Authenticatable
 
     }
 
-    function isFollowing($user_id){
+    public function isFollowing($user_id){
 
         return $this->followings->contains($user_id);
+    }
+
+    public function feed(){
+           $user_ids=Auth::user()->followings->pluck('id')->toArray();
+           array_push($user_ids,Auth::user()->id);
+           return Status::whereIn('user_id',$user_ids)
+                            ->with('user')
+                            ->orderBy('created_at','dec');
+
     }
 }
